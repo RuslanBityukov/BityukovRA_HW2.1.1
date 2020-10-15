87ad5f24d67a7569a1c468635bd0fd29902c66c4
RuslanBityukov <bitukov_ruslan@mail.ru>
2020-10-15 11:57:04 +0300

    Initial Commit

---
+++ BityukovRA_HW2.1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BityukovRA_HW2.1.1
+//
+//  Created by Руслан Битюков on 15.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
